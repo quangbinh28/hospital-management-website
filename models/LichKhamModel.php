e282a03f1bf1c15339b27f1f6f3c663a2ef2e754
quangbinh28 <135746847+quangbinh28@users.noreply.github.com>
2025-08-12 03:16:28 +0700

    Giao dien dat lich kham

--- models/LichKhamModel.php
+++ models/LichKhamModel.php
@@ -1,53 +1,26 @@
 <?php
 class LichKhamModel {
-    // Thuộc tính lịch khám
-    public $id;
-    public $benh_nhan_id;
-    public $chuyen_khoa_id;
-    public $bac_si_id;
-    public $ngay_kham;
-    public $gio_kham;
-    public $nguyen_nhan;
-    public $trang_thai; // ví dụ: đã đặt, đã khám, hủy
-
-    public function __construct($id = null, $benh_nhan_id = null, $chuyen_khoa_id = null, $bac_si_id = null, $ngay_kham = null, $gio_kham = null, $nguyen_nhan = null, $trang_thai = null) {
-        $this->id = $id;
-        $this->benh_nhan_id = $benh_nhan_id;
-        $this->chuyen_khoa_id = $chuyen_khoa_id;
-        $this->bac_si_id = $bac_si_id;
-        $this->ngay_kham = $ngay_kham;
-        $this->gio_kham = $gio_kham;
-        $this->nguyen_nhan = $nguyen_nhan;
-        $this->trang_thai = $trang_thai;
+    public function __construct() {
+        // Kết nối DB
     }
 
-    // Lấy danh sách chuyên khoa
-    public function getAllChuyenKhoa() {
-        // TODO: Gọi API hoặc query DB để lấy danh sách chuyên khoa
+    public function layDanhSachChuyenKhoa() {
+        // TODO: Truy vấn bảng Khoa
+        return [];
     }
 
-    // Lấy danh sách bác sĩ theo chuyên khoa
-    public function getBacSiByChuyenKhoa($chuyen_khoa_id) {
-        // TODO: Gọi API hoặc query DB để lấy danh sách bác sĩ
+    public function layDanhSachBacSiTheoKhoa($maKhoa) {
+        // TODO: Truy vấn bảng Bác Sĩ join Nhân Viên theo MaKhoa
+        return [];
     }
 
-    // Lấy lịch trống của bác sĩ
-    public function getLichTrongByBacSi($bac_si_id) {
-        // TODO: Gọi API hoặc query DB để lấy danh sách ngày/giờ còn trống
+    public function layDanhSachCaKhamTheoBacSi($maBS) {
+        // TODO: Truy vấn bảng Ca Làm Việc & Lịch Khám
+        return [];
     }
 
-    // Đặt lịch khám mới
-    public function datLichKham($benh_nhan_id, $chuyen_khoa_id, $bac_si_id, $ngay_kham, $gio_kham, $nguyen_nhan) {
-        // TODO: Gọi API hoặc query DB để lưu lịch khám
-    }
-
-    // Lấy danh sách lịch khám của bệnh nhân
-    public function getLichKhamByBenhNhan($benh_nhan_id) {
-        // TODO: Gọi API hoặc query DB để lấy danh sách lịch khám
-    }
-
-    // Hủy lịch khám
-    public function huyLichKham($lich_kham_id) {
-        // TODO: Gọi API hoặc query DB để hủy lịch khám
+    public function datLichKham($maBS, $ngay, $gio, $nguoiDK) {
+        // TODO: Insert vào bảng Lịch Khám
+        return true;
     }
 }
